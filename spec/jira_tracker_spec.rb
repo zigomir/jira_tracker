e5c93c445d05e605e99a59d9425ef6b7046afb2d
Oto Brglez <otobrglez@gmail.com>
2013-09-05 00:26:08 +0200

    Little change to test VERSION of module

--- spec/jira_tracker_spec.rb
+++ spec/jira_tracker_spec.rb
@@ -4,6 +4,7 @@ describe JiraTracker do
 
   context "structure" do
     subject { JiraTracker }
+    it { JiraTracker::VERSION.should match /\d+\.\d+\.\d+/ }
     it { should respond_to :run, :worklog, :find, :get_project_name_from_issue }
   end
 

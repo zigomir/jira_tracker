4ce760e74793a5e4c61943a26ea7280b2b1b2d1e
zigomir <zigomir@gmail.com>
2013-09-04 20:10:01 +0200

    config file
    -  tracking on tracking time issue for project if there is no specific issue

--- jira_tracker.gemspec
+++ jira_tracker.gemspec
@@ -8,17 +8,19 @@ Gem::Specification.new do |spec|
   spec.version       = JiraTracker::VERSION
   spec.authors       = %w(zigomir)
   spec.email         = %w(zigomir@gmail.com)
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+  spec.description   = %q{Track time on Jira spent on individual issues}
+  spec.summary       = %q{Track time on Jira spent on individual issues}
+  spec.homepage      = ''
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = %w(lib)
 
+  spec.add_runtime_dependency 'jira-ruby'
+
   spec.add_development_dependency 'bundler', '~> 1.3'
   spec.add_development_dependency 'rake'
-  spec.add_development_dependency 'jira-ruby'
+  spec.add_development_dependency 'pry'
 end
--- lib/jira_tracker.rb
+++ lib/jira_tracker.rb
@@ -1,40 +1,62 @@
 require 'jira_tracker/version'
+require 'pry'
+require 'yaml'
 require 'pp'
 require 'jira'
 
 module JiraTracker
 
-  # Consider the use of :use_ssl and :ssl_verify_mode options if running locally
-  # for tests.
-  username = ''
-  password = ''
-
-  options = {
-    :username       => username,
-    :password       => password,
-    :site           => 'https://astina.atlassian.net',
-    :context_path   => '',
-    :rest_base_path => '/rest/api/2',
-    :auth_type      => :basic
-  }
-
-  client = JIRA::Client.new(options)
-
-  # Show all projects
   begin
-    #projects = client.Project.all
-    #
-    #projects.each do |project|
-    #  puts "Project -> key: #{project.key}, name: #{project.name}"
-    #end
-
-    issue = client.Issue.find('XOYA-10')
-    #puts issue
-
-    worklog = issue.worklogs.build
-    worklog.save({'timeSpent' => '1h'})
-
-  rescue JIRA::HTTPError => e
-    puts e.response
+    CONFIG = YAML.load(File.open('jira_tracker.yml'))
+  rescue
+    p 'Setting file jira_tracker.yml not found!'
+    exit
   end
+
+  def self.run(issue, time_spent)
+    options = {
+      :username       => CONFIG['username'],
+      :password       => CONFIG['password'],
+      :site           => CONFIG['site'],
+      :context_path   => '',
+      :rest_base_path => '/rest/api/2',
+      :auth_type      => :basic
+    }
+    client = JIRA::Client.new(options)
+    JiraTracker.worklog(issue, time_spent, client)
+  end
+
+  def self.worklog(issue, time_spent, client)
+    jira_issue = nil
+    jira_issue = JiraTracker.find do
+      if issue.include?('-')
+        jira_issue = client.Issue.find(issue)
+      else
+        project_name = JiraTracker.get_project_name_from_issue(issue)
+        jql = "project = #{project_name} AND summary ~ \"#{CONFIG['time_track_issue_summary']}\""
+        jira_issue = client.Issue.jql(jql).first
+      end
+    end
+    worklog = jira_issue.worklogs.build
+    time_spent_till_now = worklog.issue.timespent / 3600
+
+    worklog.save({'timeSpent' => time_spent})
+
+    p "Thanks! #{time_spent} was added to #{jira_issue.key} - #{jira_issue.summary}"
+    p "Time spent on this issue till now #{time_spent_till_now}h"
+  end
+
+  def self.find
+    begin
+      return yield
+    rescue JIRA::HTTPError => e
+      p e.response
+      exit
+    end
+  end
+
+  def self.get_project_name_from_issue(issue)
+    issue.split('-').first.upcase
+  end
+
 end

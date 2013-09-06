de868175da0cdc9c5a487326df84204a834065cf
zigomir <zigomir@gmail.com>
2013-09-06 08:31:51 +0200

    merging pr by hand

--- lib/jira_tracker/version.rb
+++ lib/jira_tracker/version.rb
@@ -1,3 +1,3 @@
 module JiraTracker
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
 end
--- spec/jira_tracker_spec.rb
+++ spec/jira_tracker_spec.rb
@@ -1,8 +1,8 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe JiraTracker do
 
-  context "structure" do
+  context 'structure' do
     subject { JiraTracker }
     it { JiraTracker::VERSION.should match /\d+\.\d+\.\d+/ }
     it { should respond_to :run, :worklog, :find, :get_project_name_from_issue }
